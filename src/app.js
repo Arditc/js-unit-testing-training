ed8d1faedcd4d73dcc1da943728d3a39bca97c54
thedumbterminal <github@thedumbterminal.co.uk>
2016-01-15 09:54:26 +0000

    more functions

--- src/app.js
+++ src/app.js
@@ -4,12 +4,28 @@ var App = function(){
 	this.version = 1;
 };
 
-App.prototype.check = function(){
-	console.log(true);
+App.prototype.noParams = function(){
+	return true;
 };
 
-App.prototype.humanTime = function(){
-	return 1;
+App.prototype.oneParam = function(param){
+	return param * 2;
+};
+
+App.prototype.branching = function(param){
+	if(param > 5){
+		return true;
+	}
+	return false;
+};
+
+App.prototype.withCallback = function(param, callback){
+	return callback(param * 2);
+}
+
+App.prototype.withTime = function(){
+	var date = new moment();
+	return date.format('%y');
 };
 
 module.exports = App;
